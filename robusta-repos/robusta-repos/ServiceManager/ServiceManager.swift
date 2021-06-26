fb39092bed8e3d4dba14efccc19c590736a5ba16
Omar Samir <omar.mohamed2@vodafone.com>
2021-06-26 19:26:44 +0200

    models and parsing

---
+++ robusta-repos/robusta-repos/ServiceManager/ServiceManager.swift
@@ -0,0 +1,29 @@
+//
+//  ServiceManager.swift
+//  robusta-repos
+//
+//  Created by omar on 26/06/2021.
+//
+
+import Foundation
+
+class ServiceManager {
+    typealias CompletionHandler = (_ repos: [Repository]?, _ error: Error?) -> Void
+    static func downloadRepos(completionHandler:@escaping CompletionHandler) {
+        let url = URL(string: Constants.robustaEndPointsServiceURL)!
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if error == nil {
+                guard let data = data else { return }
+                do {
+                    let result = try JSONDecoder().decode([Repository].self, from: data)
+                    completionHandler(result, nil)
+                } catch {
+                    completionHandler(nil, error)
+                }
+            } else {
+                completionHandler(nil, error)
+            }
+        }
+        task.resume()
+    }
+}
