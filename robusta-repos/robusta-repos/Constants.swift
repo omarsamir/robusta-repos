e3d930c1395695a44b4fade340ea6746569e955e
Omar Samir <omar.mohamed2@vodafone.com>
2021-06-26 18:46:40 +0200

    network layer

---
+++ robusta-repos/robusta-repos/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  robusta-repos
+//
+//  Created by omar on 26/06/2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    //MARK:- Services
+    static let robustaEndPointsServiceURL = "https://api.github.com/repositories"
+}
